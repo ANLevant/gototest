7cf8d3b1e1f5dac00b9671f44f6b4e531758cf08
ANLevant <levant.alejandro@hotmail.com>
2023-04-27 00:43:22 -0400

    Add error handling for second controller too

--- src/main/java/com/test/gototechtest/rest/controller/PlayerRestController.java
+++ src/main/java/com/test/gototechtest/rest/controller/PlayerRestController.java
@@ -3,10 +3,13 @@ package com.test.gototechtest.rest.controller;
 import com.test.gototechtest.dto.CardDTO;
 import com.test.gototechtest.dto.PlayerDTO;
 import com.test.gototechtest.error.EntityDoesntExistException;
+import com.test.gototechtest.error.NotEnoughCardsInShoeExistException;
 import com.test.gototechtest.service.PlayerService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -37,5 +40,11 @@ public class PlayerRestController {
         playerDTO.setId(id);
 
         return playerService.getPlayerCards(playerDTO);
+    }
+
+    @ExceptionHandler({EntityDoesntExistException.class, NotEnoughCardsInShoeExistException.class})
+    public void handleException(HttpServletResponse res, Exception ex) throws IOException {
+        ex.printStackTrace();
+        res.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
     }
 }
